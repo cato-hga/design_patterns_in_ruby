d8dd4a93ae2a9dec6581a63437477bf8752e1513
John Cato <jchristopher.cato@gmail.com>
2024-05-02 12:12:41 -0400

    Refactor report class to use a Proc strategy instead of a strategy class

--- strategy/run_report.rb
+++ strategy/run_report.rb
@@ -6,3 +6,30 @@ report.output_report
 # it is easy to switch strategies at runtime. We simply swap out the strategy object:
 report.formatter = PlainTextFormatter.new
 report.output_report
+
+# HTML version using a proc
+HTML_FORMATTER = lambda do |context|
+  puts('<html>')
+  puts('  <head>')
+  puts("    <title>#{context.title}</title")
+  puts('  </head>')
+  puts('  <body>')
+  context.text.each do |line|
+    puts("    <p>#{line}</p>")
+  end
+  puts('  </body>')
+  puts('</html>')
+end
+
+report = Report.new &HTML_FORMATTER
+report.output_report
+
+# Plain text version using a proc object
+report = Report.new do |context|
+  puts "*** #{context.title} ***"
+  context.text.each do |line|
+    puts line
+  end
+end
+
+report.output_report
