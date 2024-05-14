4c23d85f2a74fce3bb951adaf46c9ee0c02c3901
John Cato <jchristopher.cato@gmail.com>
2024-05-14 16:20:02 -0400

    Refactor observable logic into a separate module

--- observer/employee.rb
+++ observer/employee.rb
@@ -1,31 +1,51 @@
 class Employee
-  attr_reader :name, :title
+  include Subject
+
+  attr_reader :name, :address
   attr_reader :salary
 
   def initialize(name, title, salary)
+    super()
     @name = name
     @title = title
     @salary = salary
-    @observers = []
   end
 
   def salary=(new_salary)
     @salary = new_salary
     notify_observers
   end
-
-  def notify_observers
-    @observers.each do |observer|
-      observer.update(self)
-    end
-  end
-
-  def add_observer(observer)
-    @observers << observer
-  end
-
-  def delete_observer(observer)
-    @observers.delete(observer)
-  end
 end
+# class Employee
+#   attr_reader :name, :title
+#   attr_reader :salary
+
+#   def initialize(name, title, salary)
+#     @name = name
+#     @title = title
+#     @salary = salary
+#     @observers = []
+#   end
+
+#   def salary=(new_salary)
+#     @salary = new_salary
+#     notify_observers
+#   end
+
+#   def notify_observers
+#     @observers.each do |observer|
+#       observer.update(self)
+#     end
+#   end
+
+#   def add_observer(observer)
+#     @observers << observer
+#   end
+
+#   def delete_observer(observer)
+#     @observers.delete(observer)
+#   end
+# end
+
+
 
