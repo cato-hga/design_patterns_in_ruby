41c85c1aac5f933e011d82c7789e0ef261b0c539
John Cato <jchristopher.cato@gmail.com>
2024-05-17 12:30:54 -0400

    Notify observers only if changes are present, ensure consistent state of changes.

--- observer/employee.rb
+++ observer/employee.rb
@@ -11,8 +11,23 @@ class Employee
   end
 
   def salary=(new_salary)
+    old_salary = @salary
     @salary = new_salary
-    notify_observers
+
+    if old_salary != new_salary
+      changed
+      notify_observers
+    end
+  end
+
+  def title=(new_title)
+    old_title = @title
+    @title = new_title
+
+    if old_title != new_title
+      changed = true
+      notify_observers(self)
+    end
   end
 end
 
